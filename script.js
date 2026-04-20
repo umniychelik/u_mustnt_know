120bb300c2406b4fc17c3b71b1d7e391ba1cd355
Кирилл <salnic2010@yandex.ru>
2026-04-20 17:48:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let field = document.querySelector('.field')
-
 for (let i = 0; i < 450; i += 1) {
     let cell = document.createElement('div')
     cell.classList.add('cell')
@@ -12,3 +11,16 @@ document.addEventListener('mousedown', function() {
 document.addEventListener('mouseup', function() {
     IS_CLICKED = false;
 });
+let cells = document.querySelectorAll('.cell')
+for (let i = 0; i < cells.length; i++) {
+    let cell = cells[i];
+    cell.addEventListener('click', function() {
+        if (!FILL_MODE) {
+            cell.style.backgroundColor = CURRENT_COLOR;
+        }
+    });
+    cell.addEventListener('mouseover', function() {
+        if (IS_CLICKED && !FILL_MODE) {
+            cell.style.backgroundColor = CURRENT_COLOR;
+        }
+    });
